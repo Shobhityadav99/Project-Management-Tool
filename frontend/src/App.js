8782674292e11f73266d7be3c0467b53d1c36615
Shobhityadav99 <shobhit999000@gmail.com>
2021-11-22 14:28:37 +0530

    fixed some bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Card } from "./components/Card";
 import { Navbar } from "./components/Navbar";
 import { Searchbar } from "./components/Searchbar";
+import './App.css';
 
 function App() {
   return <div className='App'>
