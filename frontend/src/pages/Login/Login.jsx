084c68aad7384af21a3bbe11303944e4f1b94e47
NishchayBhutani <bhutaninishchay9@gmail,com>
2021-11-27 16:31:12 +0530

    updated login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import "./Login.css";
+import loginImage from "../../resources/loginImage.png"
 const Login = () => {
-  return (
-    <div className="login-page">
-      <div className="navbar">
+  return ( <>
+    <div className="navbar">
         <div className="navbar-center-item1">Tutorials</div>
         <div className="navbar-center-item2">Documentation</div>
         <div className="navbar-center-item3">About Us</div>
       </div>
+        <div className="login-page">
+      <div className="login-image-container">
+        <img src={loginImage} className="login-image"/>
+      </div>
+      <div className="login-container">
       <div className="choose-container">
         <div className="choose-1 active">Login</div>
         <div className="choose-2">Register</div>
       </div>
-      <div className="login-container">
+      <div className="login-input-container">
         <div className="username">
           <label className="label">Username:</label>
           <input className="input" />
           <label className="label">Password:</label>
-          <input className="input" />
+          <input type="password" className="input" />
         </div>
         <div className="forgot-password">
           <a href="/">Forgot Password</a>
         </div>
         <button className="submit-button">Login</button>
       </div>
+      </div>
     </div>
+    </>
   );
 };
 
