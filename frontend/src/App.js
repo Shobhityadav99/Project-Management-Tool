407a228ccfd99fdc811548a76a9ab8220d174cac
Nishchay Bhutani <bhutaninishchay9@gmail.com>
2021-12-03 19:07:59 +0530

    myAccount page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
+/** @format */
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard/dashboard";
 import Login from "./pages/Login/Login";
+import MyAccount from "./pages/MyAccount/MyAccount";
 import { Project } from "./pages/Project/Project";
 import Register from "./pages/Register/Register";
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route exact path="/" element={<Dashboard />} />
-				<Route exact path="/project" element={<Project />} />
-				<Route exact path="/login" element={<Login />} />
-				<Route exact path="/register" element={<Register />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/account' element={<MyAccount />} />
+        <Route exact path='/' element={<Dashboard />} />
+        <Route exact path='/project' element={<Project />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/MyAccount/MyAccount.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {Navbar} from '../../components/Navbar';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MyAccount.css';
+import profileImage from '../../resources/pfp.jpg';
+
+const MyAccount = () => {
+
+    const [username, setUsername] = useState("Nishchay");
+    const [email, setEmail] = useState("Nishchay@test.com");
+
+    const onUsernameChangeHandler = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const onEmailChangeHandler = (e) => {
+        setEmail(e.target.value);
+    }
+
+    return (
+        <div>
+        <Navbar />
+        <div className="myaccount-container">
+            <div className="myaccount-input-container">
+            <label className="myaccount-username-label">Name:</label>
+            <br />
+            <input className="myaccount-username" value={username} onChange={onUsernameChangeHandler}/>
+            <br /><br />
+            <label className="myaccount-email-label">Email:</label>
+            <br />
+            <input className="myaccount-email" value={email} onChange={onEmailChangeHandler}/>
+            <br /><br />
+            <Button variant="primary" className="btn btn-sm">Update Info</Button>
+            <br />
+            <hr />
+            <label className="myaccount-password-change-label">Change Password:</label>
+            <br /><br />
+            <label className="myaccount-current-password-label">Enter current password:</label>
+            <br />
+            <input type="password" className="myaccount-current-password" />
+            <br /> <br />
+            <label className="myaccount-new-password-label">Enter new password:</label>
+            <br />
+            <input type="password" className="myaccount-new-password" />
+            <br /><br />
+            <label className="myaccount-retype-password-label">Retype new password:</label>
+            <br />
+            <input type="password" className="myaccount-retype-password" />
+            <br /><br />
+            <Button variant="primary" className="btn btn-sm">Update Password</Button>
+            </div>
+            <div className="myaccount-profile-image-container">
+                <img src={profileImage} className="myaccount-profile-image" />
+            </div>
+                <i className="fas fa-pen-square myaccount-profile-image-edit-icon"></i>
+                
+        </div>
+       
+            
+        </div>
+    )
+}
+
+export default MyAccount
