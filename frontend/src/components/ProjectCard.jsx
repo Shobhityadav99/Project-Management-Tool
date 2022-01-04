cd1e1ffd9836716e2f55537becc4d00404c709a3
Shobhityadav99 <shobhit999000@gmail.com>
2022-01-05 01:13:30 +0530

    minor bug fixes

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/ProjectCard.css";
 
 export const ProjectCard = props => {
   const navigate=useNavigate();
   const projects = props.projects;
+  console.log(projects);
   return (
     <>
       <div className="project-card-container">
-        {projects.map((d) => {
+        {/* {projects.map((d) => {
           return (
             <div className="project-square-container">
               <label onClick={navigate(`/project/:${d.id}`)}>{d.title}</label>
+            </div>
+          );
+        })} */}
+        {projects.forEach(project => {
+          return (
+            <div style={{border: "3px solid red"}} className="project-square-container">
+              <label onClick={navigate(`/project/:${project._id}`)}>{project.title}</label>
             </div>
           );
         })}
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -29,7 +29,7 @@ const Register = () => {
       axios.post('http://localhost:5000/user/register', receivedData)
       .then(response => {
         console.log(response);
-        navigate(`/user/dashboard/${response.data.user.id}`);
+        navigate(`/user/dashboard/${response.data._id}`);
       })
       .catch (err => {
       console.log(err);
